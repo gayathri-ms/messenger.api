f64aa222513b8342c97d87109b430ae4d9ec9546
sreenath sharma <sreenath707@dark-knight-macbook.local>
2022-08-22 15:27:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = 8000;
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection is established id : ", socket.id);
+  socket.on("message", (data) => {
+    console.log("message event : ", data.msg);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`server listenning on port ${port}`);
+});
